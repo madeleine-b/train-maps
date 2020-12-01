1f500474610beceb45a22f96671d5817103b9ae8
Madeleine Barowsky <mbarowsky@g.harvard.edu>
2020-12-01 15:35:52 -0500

    Merged Wael's changes into the python file and made generate_adj_matrix take parameters for optionally also writing the adj matrix to a file

--- make_mbta_adj_mat.py
+++ make_mbta_adj_mat.py
@@ -5,6 +5,9 @@ import pandas as pd
 import json
 import time
 
+import itertools
+from scipy.optimize import lsq_linear
+
 def generate_route_json():
 	s = requests.Session()
 	s.headers.update({'accept': 'application/vnd.api+json'})
@@ -75,34 +78,125 @@ def dedup_trips():
 		with open("mbta_trips_deduped", "w") as f2:
 			json.dump({str(k) : v for k, v in deduped_trip_stop_dict.items()}, f2)
 
+def generate_adj_matrix(use_readable_station_names=False, write_to_file=True, filename=None):
+	stop_info_index = 1 if use_readable_station_names else 0
+	all_places = []
+	trip_stop_dict = None
+	with open("mbta_trips_deduped", "r") as f:
+		trip_stop_dict = json.load(f)
+		for k, v in trip_stop_dict.items():
+			for place in v:
+				all_places.append(place[stop_info_index])
+		all_places = np.unique(np.array(all_places))
+		# Verified by hand that this gets all 118 subway stations :sweat_smile:
 
-all_places = []
-trip_stop_dict = None
-with open("mbta_trips_deduped", "r") as f:
-	trip_stop_dict = json.load(f)
-	for k, v in trip_stop_dict.items():
-		for place in v:
-			all_places.append(place[0])
-	all_places = np.unique(np.array(all_places))
-	# Verified by hand that this gets all 118 subway stations :sweat_smile:
+	adjacency_mat = np.zeros((len(all_places), len(all_places))).astype('int')
+	df = pd.DataFrame(adjacency_mat, columns=all_places, index=all_places)
 
-adjacency_mat = np.zeros((len(all_places), len(all_places))).astype('int')
-df = pd.DataFrame(adjacency_mat, columns=all_places, index=all_places)
+	for route_ids, route in trip_stop_dict.items():
+		for i in range(len(route)):
+			place_id = route[i][stop_info_index]
+			if i != len(route) - 1:
+				next_place_id = route[i + 1][stop_info_index]
+				df[place_id][next_place_id] = 1
+				df[next_place_id][place_id] = 1
+			if i != 0:
+				prev_place_id = route[i - 1][stop_info_index]
+				df[place_id][prev_place_id] = 1
+				df[prev_place_id][place_id] = 1
 
-for route_ids, route in trip_stop_dict.items():
-	for i in range(len(route)):
-		place_id = route[i][0]
-		if i != len(route) - 1:
-			next_place_id = route[i + 1][0]
-			df[place_id][next_place_id] = 1
-			df[next_place_id][place_id] = 1
-		if i != 0:
-			prev_place_id = route[i - 1][0]
-			df[place_id][prev_place_id] = 1
-			df[prev_place_id][place_id] = 1
-print(df)
+	if write_to_file:
+		if filename is None:
+			filename = "mbta_adj_mat"
+			if use_readable_station_names:
+				filename += "_readable"
+		with open(filename, "w") as f:
+			df.to_json(f)
+	return df
 
-with open("mbta_adj_mat", "w") as f:
-	df.to_json(f)
 
+M = generate_adj_matrix(write_to_file=False).to_numpy()
+
+N = 300 # assumed total number of trains in the system
+n = M.shape[0]
+pi = [1/n]*n + np.random.random(n)/N
+pi = pi/sum(pi) # made-up target stationary distribution
+
+Mp = M # modified adjacency so that the Markov chain is ergodic
+d = np.sum(Mp,axis=1)
+print('degree:', d)
+print('\n #nonzero entries:', np.sum(d))
+
+boundary = np.argwhere(d==1).flatten()
+for b in boundary:
+    Mp[b,b] = 1
+d = np.sum(Mp,axis=1)
+print('\n modified degrees', d)
+
+w_supp = np.argwhere(Mp!=0)
+n_w = len(w_supp)
+
+T = [[i,j] for i,j in itertools.product(range(n),range(n)) if Mp[i,j] != 0] # the coordinates we care about (~ 300 out of ~ 140k)
+
+TI = [] # neighbor list
+q = 0
+for i in range(n):
+    TI.append([T[k][1] for k in range(q,q+d[i])])
+    q += d[i]
+
+print(TI)
+
+TI_lengths = [0]
+q = 0
+for t in TI:
+    q += len(t)
+    TI_lengths.append(q)
+
+B_pi = np.zeros((n,n_w))
+for i in range(n):
+    B_pi[i,TI_lengths[i]:TI_lengths[i+1]] = [pi[j] for j in TI[i]]
+
+B_1 = np.zeros((n,n_w))
+for j in range(n):
+    I = np.array(TI[j])
+    for i in I:
+        B_1[j,TI_lengths[i]+np.argwhere(np.array(TI[i])==j)[0,0]] = 1
+
+zeta = 1
+B = np.vstack([B_pi,zeta*B_1])
+c = np.hstack([pi,zeta*np.ones(n)])
+LB = (1/N)*np.ones(n_w)
+
+x2 = lsq_linear(B, c, bounds=(LB, np.inf), # solution
+                method='trf', tol=1e-10, lsq_solver=None, lsmr_tol=None, 
+                max_iter=None, verbose=0).x
+
+print((np.matmul(B,x2) - c)/c)
+
+def weight(i,j,x,TI,TI_lengths):
+    t = np.argwhere(np.array(TI[i]) == j).flatten()
+    if len(t):
+        return x[TI_lengths[i]+t[0]]
+    return 0
+
+print('e.g.', weight(0,116,x2,TI,TI_lengths))
+
+for i in range(n):
+    print(sum(x2[TI_lengths[i]:TI_lengths[i+1]]))
+
+X = np.zeros((n,n))
+for i,j in itertools.product(range(n),range(n)):
+    X[i,j] = weight(i,j,x2,TI,TI_lengths)
+
+p0 = np.array([1]+[0]*(n-1)).reshape(n,)
+p = p0
+t = 20000
+for i in range(t):
+    p = np.matmul(X,p)
+print(p)
+print(np.linalg.norm(p-pi,1))
+			
+
+
+	
 			

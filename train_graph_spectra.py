af7b476035ee6dd13ae54aaa4aa970fb77360cf9
Madeleine Barowsky <mbarowsky@g.harvard.edu>
2020-12-12 18:06:08 -0500

    Clarify computation of graph and update figure

--- train_graph_spectra.py
+++ train_graph_spectra.py
@@ -27,16 +27,20 @@ def spectra(file):
 	assert np.allclose(np.sum(random_walk, axis=0), 1), "Random walk isn't stochastic"
 
 	laplacian = degree - adj_mat
+	normalized_laplacian = np.matmul(np.matmul(d_inv, laplacian), d_inv)
 
 	omegas, w = np.linalg.eig(random_walk)
 	lambdas, v = np.linalg.eig(laplacian)
+	nus, v2 = np.linalg.eig(normalized_laplacian)
 
 	omegas = sorted(np.real(omegas), reverse=True)
-	lambdas = sorted(np.real(lambdas), reverse=True)
-	print(lambdas[1])
+	lambdas = sorted(np.real(lambdas))
+	nus = sorted(np.real(nus))
+	print("lambda_2", lambdas[1])
+	print("nu_2", nus[1])
 
 	w_pi = max(omegas[1], -omegas[-1])
-	print(w_pi)
+	print("w_pi", w_pi)
 
 
 def perf_metrics():
@@ -45,8 +49,10 @@ def perf_metrics():
 		wait_time_avg = metrics18_19["otp_numerator"].to_numpy().sum() / metrics18_19["otp_denominator"].to_numpy().sum()
 		print("average passenger wait time: ", wait_time_avg)
 
-	x = np.linspace(0, 1, 100)
-	y = np.array([1/(np.log(12.56*456/pt)*0.0012/(0.0063*np.log(456/pt))) for pt in x])
+	x = np.linspace(0, 1, 100)[1:]
+	mta_mixing_time = lambda var: np.log(5728/var)/0.0063
+	mbta_mixing_time = lambda var: np.log(456/var)/0.0012
+	y = np.array([mbta_mixing_time(pt)/mta_mixing_time(pt) for pt in x])
 	plt.plot(x, y)
 	plt.xlabel("Epsilon")
 	plt.ylabel("Ratio of MBTA to MTA mixing time")
